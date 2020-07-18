a875c53668648cc7cd27b18db02d64d1a8c8b0f8
Jason Kim <thehesedlife@gmail.com>
2020-07-18 12:55:12 -0700

    basic ReviewCards component - updated some methods - updated render method to draw carousel elements

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,40 +7,47 @@ import Nav from './nav'
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { view: 'view-cards', cards: [], activeCard: {} }
+    this.state = { view: 'view-cards', cards: [], activeCard: { index: 0, card: {}} }
     this.setView = this.setView.bind(this)
     this.addCard = this.addCard.bind(this)
     this.setActiveCard = this.setActiveCard.bind(this)
   }
 
   setView(value) {
-
     this.setState({ view: value })
   }
 
   getView() {
     switch(this.state.view) {
       case 'create-card':
-        return <CreateCard newCard={this.addCard} setPosition={this.setView}/>;
+        return <CreateCard newCard={this.addCard} setPosition={this.setView}/>
       case 'review-cards':
-        return <ReviewCards activeCard={this.state.activeCard} setActive={this.setActiveCard}/>;
+        return <ReviewCards activeCard={this.state.activeCard} setActive={this.setActiveCard}/>
       case 'view-cards':
-        return <ViewCards cards={this.state.cards}/>;
+        return <ViewCards cards={this.state.cards}/>
       default:
         return null;
     }
   }
 
   saveCard() {
-    localStorage.setItem('flash-cards', JSON.stringify(this.state.cards));
+    localStorage.setItem('flash-cards', JSON.stringify(this.state.cards))
   }
 
   addCard(card) {
-    this.setState({ cards: this.state.cards.concat(card) }, () => this.saveCard());
+    this.setState({ cards: this.state.cards.concat(card) }, () => this.saveCard())
   }
 
   setActiveCard(cardIndex) {
-    this.setState({activeCard: this.state.cards[cardIndex]})
+    if (this.state.cards.length === 0) return;
+
+    if (cardIndex > this.state.cards.length - 1) {
+      cardIndex = 0
+    } else if (cardIndex < 0) {
+      cardIndex = this.state.cards.length - 1
+    }
+
+    this.setState({ activeCard: { index: 0, card: this.state.cards[cardIndex]}})
   }
 
   render() {
--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 
 export default class ReviewCards extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { displaySide: "question" }
+    this.nextCard = this.nextCard.bind(this)
+    this.previousCard = this.previousCard.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.setActive(0)
+  }
+
+  nextCard() {
+    const nextIndex = this.props.setActive.index + 1;
+    this.props.setActive(nextIndex)
+    this.setState({displaySide: "question"})
+
+  }
+
+  previousCard() {
+    const prevIndex = this.props.setActive.index - 1;
+    this.props.setActive(prevIndex)
+    this.setState({displaySide: "question"})
+  }
+
   render() {
+    const qna = this.state.displaySide === "question" ? this.props.activeCard.card.question : this.props.activeCard.card.answer
+
     return (
       <div className="review-card-container">
         <h1 className="text-center mb-3">Review Cards</h1>
+        <div className="review-card">
+          <h2>{qna}</h2>
+          <a href="#" className="previous" onClick={this.previousCard}>&#10094;</a>
+          <a href="#" className="next" onClick={this.nextCard}>&#10095;</a>
+        </div>
       </div>
     )
   }
